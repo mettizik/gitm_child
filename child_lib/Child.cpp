957907656e4f5b06eb4322607a5b03987fd9c9f8
Andrey Mokych <mokych.andrey@apriorit.com>
2017-09-29 09:45:37 +0300

    Implemented function

---
+++ child_lib/Child.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "Child.h"
+
+int child::SumItems(int a, int b)
+{
+    return a + b;
+}
---
+++ child_lib/Child.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace child
+{
+    int SumItems(int a, int b);
+}
